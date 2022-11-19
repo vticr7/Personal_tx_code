43e5eca8ebfcec2221b7fea156a1cb4ee9d0b046
vticr7 <vti9227@gmail.com>
2022-11-19 13:18:31 +0530

    second

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 import time as t
-
+import numpy as np
 from googleapiclient.discovery import build
 import pandas as pd
 import seaborn as sns
 import csv
+import matplotlib.pyplot as pylot
+import statistics as st
 api_key = 'AIzaSyDgqj4wCUTvAh48KvYAvHPgPqKDYM9LS0E'
 
 youtube = build('youtube', 'v3', developerKey=api_key)
@@ -18,11 +20,33 @@ def get_video_stats(youtube):
     return req_data
 
 
-for i in range(100):
-    with open("traddd.csv", 'a') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        current_time = t.strftime("%H:%M:%S")
-        csvwriter.writerow([i, current_time, get_video_stats(youtube)])
-        print(str(current_time)+"  "+str(get_video_stats(youtube)))
-        csvfile.close()
-        t.sleep(60*3)
+# for i in range(100):
+#     with open("traddd.csv", 'a') as csvfile:
+#         csvwriter = csv.writer(csvfile)
+#         current_time = t.strftime("%H:%M:%S")
+#         csvwriter.writerow([i, current_time, get_video_stats(youtube)])
+#         print(str(current_time)+"  "+str(get_video_stats(youtube)))
+#         csvfile.close()
+#         t.sleep(60*3)
+
+
+data = pd.read_csv("traddd.csv", error_bad_lines=False)
+print(data.shape)
+print(data.head())
+
+y = data["Views"].values
+x = data["Index"].values
+mean_views_y = float(0)
+mean_Index_x = 0
+
+for i in range(len(x)-1):
+    mean_views_y += y[i]/len(x)
+    mean_Index_x += x[i]/len(x)
+
+
+numer = 0
+denom = 0
+for i in range(len(x)-1):
+
+    numer = numer+(x[i]-mean_Index_x)*(y[i]-mean_views_y)
+    denom += (x[i]-mean_Index_x)**2
--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-if (i != 0):
-            f = open("traddd.csv")
-            f.writerow([i, current_time, get_video_stats(youtube)])
-
-            f.close()
